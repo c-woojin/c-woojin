f7f7417d2ca242debba0089d0b979e460a6b8a17
Will <cwoojin@outlook.com>
2023-08-24 15:30:11 +0900

    fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "075c1e58cb06ff8eb3c39f9a8e4393bc",
 "favicon.ico": "6de7564c9aecaab6b99737434c505fb3",
-"index.html": "a8c57692cda3ac1ad1c57167549823ee",
-"/": "a8c57692cda3ac1ad1c57167549823ee",
-"main.dart.js": "2f4ede466f5429e0e2bb717c1896a5e1",
+"index.html": "626c27f8d213443122ec43e8c9304053",
+"/": "626c27f8d213443122ec43e8c9304053",
+"main.dart.js": "31b46f0f09e81dab941addc49a7db3ce",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "icons/Icon-192.png": "0bde8f63f28e0bc15a8e5d95f6a590f9",
 "icons/Icon-maskable-192.png": "0bde8f63f28e0bc15a8e5d95f6a590f9",
